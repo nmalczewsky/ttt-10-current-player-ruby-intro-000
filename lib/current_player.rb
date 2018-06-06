0d566529b53b389bcc26d01ff109d573b72718bc
Nicholas Malczewsky <n.malczewsky@gmail.com>
2018-06-06 20:10:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,9 +11,6 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  elsif turn_count(board).odd?
-    return "O"
-  end
+  turn_count(board).even? return "X"
+  turn_count(board).odd?  return "O"
 end
